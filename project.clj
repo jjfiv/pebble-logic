3288287b30fc08fa8cb68dd28f4d501962974d70
John Foley <jfoley@cs.umass.edu>
2013-12-04 09:39:55 -0500

    Simple drawing with clojure, seesaw

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  ;[org.ow2.sat4j/org.ow2.sat4j.core "2.3.4"]
                  [net.sourceforge.jeuclid/jeuclid-core "3.1.9"]
+                 [seesaw "1.4.4"]
                  ;[net.sf.jung/jung2 "2.0.1" :extension "pom"]
                  ;[net.sf.jung/jung-api "2.0.1"]
                  ]
--- src/pebble/core.clj
+++ src/pebble/core.clj
@@ -1,17 +1,32 @@
 (ns pebble.core
+  (:use seesaw.core
+        seesaw.graphics
+        seesaw.color)
   (:gen-class))
 
-(defn relation [id arity]
-  {:id id :arity arity)
 
-(def l1 {
-          :size 1
-          :relations #{ (relation "E" 2) }
-         })
-(def l3 {
-         :size 3
-         :relations #{ (relation "E" 2) }
-         })
+(def l5 
+  {
+   :name "l5"
+   :size 5
+   :relations #{ {:name "E" :arity 2 :entries {[0,1],[1,2],[2,3],[3,4]} } }
+   :constants { "s" 0 "t" 4 }
+   })
+
+(defn paint-checkerboard [c g]
+  (let [w (.getWidth c)
+        h (.getHeight c)]
+    (draw g (rect 0 0 (/ w 2) (/ h 2)) (style :background "#ff0000"))
+    (draw g (rect (/ w 2) (/ h 2) w h) (style :background "#ff0000"))))
+
+
+(defn run []
+  (doto (frame
+          :title "EF-games" :width 640 :height 480
+          :content
+          (canvas :id :canvas :background "#ffffff" :paint paint-checkerboard))
+    (show!)))
+
 
 (defn -main [& args]
   (println "Hello, World!"))
